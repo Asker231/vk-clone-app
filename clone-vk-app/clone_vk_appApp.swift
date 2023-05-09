75426332292838eea225b8506d7517343c01e536
Asker <kotsevasker0@gmail.com>
2023-05-09 08:04:33 +0300

    Initial Commit

---
+++ clone-vk-app/clone_vk_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  clone_vk_appApp.swift
+//  clone-vk-app
+//
+//  Created by ASKER on 09.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct clone_vk_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
